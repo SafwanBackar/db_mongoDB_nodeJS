158c0b3f264a5f74698a9f5d8d58ba87905637ef
Safwan Backar <sbackar29@gmail.com>
2022-05-09 13:14:16 +0530

    extra

--- create-db.js
+++ create-db.js
@@ -1,3 +1,13 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/my_db";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
+
+
 // const mongo = require('mongodb')
 // const url = 'mongodb://localhost:27017/demo_db'
 
@@ -23,12 +33,26 @@
 // })
 
 
-const mongodb = require('mongodb')
-const uri = 'mongodb://localhost:27017';
-const client = new mongodb.MongoClient(uri)
+// const mongodb = require('mongodb')
+// const uri = 'mongodb://localhost:27017';
+// const client = new mongodb.MongoClient(uri)
 
-client.connect((err) => {
-    if (!err) {
-        console.log('Database created successfully')
-    }
-})
+// client.connect((err) => {
+//     if (!err) {
+//         console.log('Database created successfully')
+//     }
+// })
+
+
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+
+// MongoClient.connect(url, function (err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     dbo.createCollection("customers", function (err, res) {
+//         if (err) throw err;
+//         console.log("Collection created!");
+//         db.close();
+//     });
+// });
